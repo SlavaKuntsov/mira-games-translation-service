9ab7d789fdc4781852c5c1a190420266ba0ef6c7
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-07-02 22:55:33 +0300

    feat: paginated api response

--- src/server/SharedKernel/Common/Results/ApiResponse.cs
+++ src/server/SharedKernel/Common/Results/ApiResponse.cs
@@ -1,6 +1,12 @@
 ﻿namespace Common.Results;
 
-public class ApiResponse<T>(T data)
+public record ApiResponse<T>(T Data);
+
+public record ApiResponsePaginated<T>(
+	T Data,
+	int CurrentPage,
+	int PageSize,
+	int TotalRecords) : ApiResponse<T>(Data)
 {
-	public T Data { get; } = data;
+	public int TotalPages { get; } = (int)Math.Ceiling(TotalRecords / (double)PageSize);
 }
